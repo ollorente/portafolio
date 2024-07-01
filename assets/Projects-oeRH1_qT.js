789523c196a4b550d2e416b9e49906168168eb63
Óscar Llorente <llorente_oscar@hotmail.com>
2024-07-01 01:29:59 -0500

    Updates

---
+++ assets/Projects-oeRH1_qT.js
@@ -0,0 +1,1 @@
+import{c as t,t as e,o}from"./index-ClQ6t8LD.js";const s={class:"text-2xl font-semiblod"},c="Proyectos",l={__name:"Projects",setup(r){return(a,n)=>(o(),t("h1",s,e(c)))}};export{l as default};
