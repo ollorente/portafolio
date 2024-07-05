18d9b744df53b51c2ea914c1c73740ad1164d4cc
Óscar Llorente <llorente_oscar@hotmail.com>
2024-07-05 15:29:41 -0500

    Agregando título de página

--- src/router/index.js
+++ src/router/index.js
@@ -1,36 +1,58 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
+const BASE_URL_TITLE = '© Ollorente portafolio'
+
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: () => import(/* webpackChunkName "home" */ '@/views/Home.vue')
+    component: () => import(/* webpackChunkName "home" */ '@/views/Home.vue'),
+    meta: {
+      title: `Home ${BASE_URL_TITLE}`
+    }
   },
   {
     path: '/contacto',
     name: 'Contact',
-    component: () => import(/* webpackChunkName "contact" */ '@/views/Contact.vue')
+    component: () => import(/* webpackChunkName "contact" */ '@/views/Contact.vue'),
+    meta: {
+      title: `Contacto ${BASE_URL_TITLE}`
+    }
   },
   {
     path: '/proyectos',
     name: 'Projects',
-    component: () => import(/* webpackChunkName "projects" */ '@/views/Projects.vue')
+    component: () => import(/* webpackChunkName "projects" */ '@/views/Projects.vue'),
+    meta: {
+      title: `Proyectos ${BASE_URL_TITLE}`
+    }
   },
   {
     path: '/proyecto/:id',
     name: 'Project',
-    component: () => import(/* webpackChunkName "projects" */ '@/views/Project.vue')
+    component: () => import(/* webpackChunkName "projects" */ '@/views/Project.vue'),
+    meta: {
+      title: `Proyecto ${BASE_URL_TITLE}`
+    }
   },
   {
     path: '/:pathMacth(.*)*',
     name: 'NotFound',
-    component: () => import(/* webpackChunkName "not-found" */ '@/views/NotFound.vue')
+    component: () => import(/* webpackChunkName "not-found" */ '@/views/NotFound.vue'),
+    meta: {
+      title: `Página no encontrada ${BASE_URL_TITLE}`
+    }
   },
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title
+  next()
 })
 
 export default router
