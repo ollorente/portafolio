5b2fbd7f202386637ecbd0337c728d704c65aa77
Óscar Llorente <llorente_oscar@hotmail.com>
2024-07-05 15:28:22 -0500

    Creando archivo

---
+++ src/components/UI/Img/Highlight.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps(["item"])
+</script>
+
+<template>
+  <RouterLink :to="{ name: 'Proyect', params: { id: item.slug } }" class="text-center">
+    <img :src="item.image" alt="Soporte" class="w-[77px] aspect-square rounded-full border-[3px]">
+    <p>
+      item.tilte
+    </p>
+  </RouterLink>
+</template>
