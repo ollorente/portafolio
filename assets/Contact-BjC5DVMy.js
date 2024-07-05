a1a2daf7acc577f5ce98886c547eadf84d3d1d18
Óscar Llorente <llorente_oscar@hotmail.com>
2024-07-05 15:30:39 -0500

    Updates

--- assets/Contact-BjC5DVMy.js
+++
@@ -1,1 +0,0 @@
-import{c as t,t as o,o as e}from"./index-ClQ6t8LD.js";const s={class:"text-2xl font-semiblod"},c="Contacto",l={__name:"Contact",setup(a){return(n,r)=>(e(),t("h1",s,o(c)))}};export{l as default};
--- assets/Home-CJsEZj-O.js
+++
@@ -1,1 +0,0 @@
-import{c as t,t as e,o}from"./index-ClQ6t8LD.js";const s={class:"text-2xl font-semiblod"},c="Home",l={__name:"Home",setup(a){return(n,r)=>(o(),t("h1",s,e(c)))}};export{l as default};
