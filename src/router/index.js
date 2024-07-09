d95191f8787c6922c0908b0ab966a017f2d138b0
Óscar Llorente <llorente_oscar@hotmail.com>
2024-07-09 14:36:20 -0500

    Organizando rutas

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
-const BASE_URL_TITLE = '© Ollorente portafolio'
+const BASE_URL_TITLE = '© Oscar Llorente'
 
 const routes = [
   {
@@ -9,14 +9,6 @@ const routes = [
     component: () => import(/* webpackChunkName "home" */ '@/views/Home.vue'),
     meta: {
       title: `Home ${BASE_URL_TITLE}`
-    }
-  },
-  {
-    path: '/contacto',
-    name: 'Contact',
-    component: () => import(/* webpackChunkName "contact" */ '@/views/Contact.vue'),
-    meta: {
-      title: `Contacto ${BASE_URL_TITLE}`
     }
   },
   {
@@ -30,9 +22,49 @@ const routes = [
   {
     path: '/proyecto/:id',
     name: 'Project',
-    component: () => import(/* webpackChunkName "projects" */ '@/views/Project.vue'),
+    component: () => import(/* webpackChunkName "project" */ '@/views/Project.vue'),
     meta: {
       title: `Proyecto ${BASE_URL_TITLE}`
+    }
+  },
+  {
+    path: '/accesibilidad',
+    name: 'Accessibility',
+    component: () => import(/* webpackChunkName "accessibility" */ '@/views/Contact.vue'),
+    meta: {
+      title: `Accesibilidad ${BASE_URL_TITLE}`
+    }
+  },
+  {
+    path: '/cookies',
+    name: 'Cookies',
+    component: () => import(/* webpackChunkName "cookies" */ '@/views/Contact.vue'),
+    meta: {
+      title: `Cookies ${BASE_URL_TITLE}`
+    }
+  },
+  {
+    path: '/mas',
+    name: 'More',
+    component: () => import(/* webpackChunkName "more" */ '@/views/Contact.vue'),
+    meta: {
+      title: `Más ${BASE_URL_TITLE}`
+    }
+  },
+  {
+    path: '/privacidad',
+    name: 'Privacy',
+    component: () => import(/* webpackChunkName "privacy" */ '@/views/Contact.vue'),
+    meta: {
+      title: `Privacidad ${BASE_URL_TITLE}`
+    }
+  },
+  {
+    path: '/publicidad',
+    name: 'Advertising',
+    component: () => import(/* webpackChunkName "advertising" */ '@/views/Contact.vue'),
+    meta: {
+      title: `Publicidad ${BASE_URL_TITLE}`
     }
   },
   {
