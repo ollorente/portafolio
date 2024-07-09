0aad868a7d4e05be099c4e0104fb584255c3cade
Óscar Llorente <llorente_oscar@hotmail.com>
2024-07-09 14:40:26 -0500

    Cambiando router

--- src/components/UI/Img/Highlight.vue
+++ src/components/UI/Img/Highlight.vue
@@ -3,7 +3,7 @@ defineProps(["item"])
 </script>
 
 <template>
-  <RouterLink :to="{ name: 'Proyect', params: { id: item.slug } }" class="text-center">
+  <RouterLink :to="{ name: 'Project', params: { id: item.slug } }" class="text-center">
     <img :src="item.image" alt="Soporte" class="w-[77px] aspect-square rounded-full border-[3px]">
     <p>
       item.tilte
