cf20ddec9d16b17bc6d6eb6e1119a3a9cb412408
Óscar Llorente <llorente_oscar@hotmail.com>
2024-07-09 14:31:09 -0500

    Quitando menú

--- src/app.vue
+++ src/app.vue
@@ -1,13 +1,7 @@
 <script setup></script>
 
 <template>
-  <div class="container mx-auto">
-    <div class="text-center p-4 flex flex-row items-center gap-2">
-      <router-link :to="{ name: 'Home' }" class="text-xl">Home</router-link> |
-      <router-link :to="{ name: 'Projects' }" class="text-xl">Proyectos</router-link> |
-      <router-link :to="{ name: 'Contact' }" class="text-xl">Contacto</router-link>
-    </div>
-
+  <div class="">
     <router-view></router-view>
   </div>
 </template>
