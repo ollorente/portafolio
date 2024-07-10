0cdfe46f447861034587afdbead26c013ff8d1ae
Óscar Llorente <llorente_oscar@hotmail.com>
2024-07-09 19:04:24 -0500

    Creando archivo

---
+++ src/components/UI/Main/Header.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="sticky top-0 px-4 py-3 bg-white/80 backdrop-blur-md dark:bg-dim-900/80 border-b">
+    <h1 class="text-2xl font-bold text-gray-800 dark:text-gray-100">
+      <slot></slot>
+    </h1>
+  </div>
+</template>
