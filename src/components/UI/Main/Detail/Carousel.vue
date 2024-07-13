e18c0fd65dd607018fd5be755c859eb02170adb7
Óscar Llorente <llorente_oscar@hotmail.com>
2024-07-12 22:15:48 -0500

    Creando archivo

---
+++ src/components/UI/Main/Detail/Carousel.vue
@@ -0,0 +1,17 @@
+<script setup>
+const props = defineProps({
+  images: {
+    type: Array,
+    default: [],
+  }
+})
+</script>
+
+<template>
+  <div v-for="image in props.images" :key="image.id" class="w-full bg-gray-950 aspect-square flex flex-row m-0"
+    :class="twitterBorderColor">
+    <div class="w-full h-full">
+      <img :src="image.url" class="w-full aspect-square object-contain rounded-2xl" />
+    </div>
+  </div>
+</template>
