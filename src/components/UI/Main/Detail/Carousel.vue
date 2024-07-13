cbd2892a43b45c13803208961e409bdca276cc3b
Óscar Llorente <llorente_oscar@hotmail.com>
2024-07-12 22:36:18 -0500

    Ajustando tamaños

--- src/components/UI/Main/Detail/Carousel.vue
+++ src/components/UI/Main/Detail/Carousel.vue
@@ -8,10 +8,10 @@ const props = defineProps({
 </script>
 
 <template>
-  <div v-for="image in props.images" :key="image.id" class="w-full bg-gray-950 aspect-square flex flex-row m-0"
+  <div v-for="image in props.images" :key="image.id" class="w-full bg-gray-900 flex flex-row m-0"
     :class="twitterBorderColor">
     <div class="w-full h-full">
-      <img :src="image.url" class="w-full aspect-square object-contain rounded-2xl" />
+      <img :src="image.url" class="w-full aspect-square object-contain" />
     </div>
   </div>
 </template>
