6556e225234f9d1bfbbf3b8391f7704b8650cf73
Óscar Llorente <llorente_oscar@hotmail.com>
2024-07-19 00:48:58 -0500

    Ajustando lógica y diseño

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,29 +2,29 @@
 import { onMounted, ref, watch } from "vue"
 import { useRoute } from "vue-router"
 import UIMainPost from "@/components/UI/Main/Post.vue"
-import UIGrafListFeed from "@/components/UI/Graf/ListFeed.vue"
-import useGraf from "@/composables/useGraf.js"
+import UIPostListFeed from "@/components/UI/Post/ListFeed.vue"
+import usePost from "@/composables/usePost.js"
 
-const { GetAllGrafs } = useGraf()
+const { GetAllPosts } = usePost()
 const route = useRoute()
 const title = "Home"
 const Error = ref()
 const isError = ref(false)
 const isLoading = ref(true)
-const limit = ref(10)
+const limit = ref(20)
 const page = ref(0)
-const items = ref([])
+const items = ref()
 
 const getItems = async () => {
   isLoading.value = true
 
   try {
-    const data = await GetAllGrafs({ limit: limit.value, page: page.value })
+    const data = await GetAllPosts({ limit: limit.value, page: page.value })
 
     items.value = data.map((e) => {
       return {
         ...e,
-        url: { name: 'Project', params: { id: e.id } }
+        url: `/${e.type}/${e.id}`
       }
     })
   } catch (error) {
@@ -42,7 +42,7 @@ onMounted(() => getItems())
 <template>
   <UIMainPost :title="title">
 
-    <UIGrafListFeed :grafs="items" compact />
+    <UIPostListFeed :posts="items" compact />
 
   </UIMainPost>
 </template>
