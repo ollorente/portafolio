d848d85873ece21b401d2aa42cb94ec8da53013e
Óscar Llorente <llorente_oscar@hotmail.com>
2024-07-22 23:10:45 -0500

    Ajustando diseño y lógica

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -2,43 +2,31 @@
 import { onMounted, ref, watch } from "vue"
 import { useRoute } from "vue-router"
 import UIMainDetail from "@/components/UI/Main/Detail/index.vue"
-import UIMainDetailCarousel from "@/components/UI/Main/Detail/Carousel.vue"
 import UIMainDetailContent from "@/components/UI/Main/Detail/Content.vue"
-import useGraf from "@/composables/useGraf.js"
-import useMediaFile from "@/composables/useMediaFile.js"
+import usePost from "@/composables/usePost.js"
 
-const { GetOneGraf } = useGraf()
-const { GetAllMediaFiles } = useMediaFile()
+const { GetOnePost } = usePost()
 const route = useRoute()
 const ID = route.params.id
 const title = "Proyecto"
 const Error = ref()
 const isError = ref(false)
 const isLoading = ref(true)
-const limit = ref(10)
-const page = ref(0)
 const item = ref()
-const mediaFiles = ref()
 
 const getItem = async () => {
   isLoading.value = true
 
   try {
-    const data = await GetOneGraf(ID)
+    const data = await GetOnePost(ID)
 
     item.value = data
-    await getMediaFiles()
   } catch (error) {
     isError.value = true
     Error.value = error
   } finally {
     isLoading.value = false
   }
-}
-
-const getMediaFiles = async () => {
-  const data = await GetAllMediaFiles(ID, { limit: limit.value, page: page.value })
-  mediaFiles.value = data
 }
 
 watch(() => route.fullPath, () => getItem())
@@ -49,10 +37,9 @@ onMounted(() => getItem())
   <UIMainDetail :back="{ name: 'Projects' }" :title="title">
     <div class="">
 
-      <UIMainDetailCarousel :images="mediaFiles" />
-
       <UIMainDetailContent>
-        {{ item?.text }}
+        <h1 v-if="item?.title" class="text-2xl font-semibold mb-4">{{ item?.title }}</h1>
+        <div v-html="item?.content"></div>
       </UIMainDetailContent>
 
     </div>
